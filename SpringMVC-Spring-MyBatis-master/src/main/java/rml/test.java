64b6cbf44e49c605f09fb82fd6103b2d69db7e98
rtwangpy <rtwangpy@yonyou.com>
2019-11-07 17:01:33 +0800

    develop-V1 333

--- SpringMVC-Spring-MyBatis-master/src/main/java/rml/test.java
+++ SpringMVC-Spring-MyBatis-master/src/main/java/rml/test.java
@@ -2,6 +2,8 @@ package rml;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(333);
+        System.out.println(333);
+        System.out.println(333);
     }
 }
